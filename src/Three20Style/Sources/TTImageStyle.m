6c96f9e05e4dadf3c95b6db062dbcebcb159ee55
Vladimir Grichina <vgrichina@componentix.com>
2011-06-01 21:02:32 +0300

    Made TTImageStyle render defaultImage when there is no image available

--- src/Three20Style/Sources/TTImageStyle.m
+++ src/Three20Style/Sources/TTImageStyle.m
@@ -159,6 +159,10 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)draw:(TTStyleContext*)context {
   UIImage* image = [self imageForContext:context];
+  if (!image) {
+    image = self.defaultImage;
+  }
+
   if (image) {
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     CGContextSaveGState(ctx);
